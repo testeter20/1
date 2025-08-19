a3251884fc242db50e750adf4d43fe2bac17cb7c
testeter20 <yilankara7452@gmail.com>
2025-08-19 10:14:32 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { fileURLToPath } from 'url';
 
 export default defineConfig({
   plugins: [react()],
   root: '.',
+  base: '/omc-website/', // GitHub repository adınız
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
